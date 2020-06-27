de420ca132f19bedd732d929f72de7605846046b
Danish-Asghar2909 <danish.2909@icloud.com>
2020-06-26 17:08:47 -0700

    Updates

---
+++ precache-manifest.24141b7c7e3ff43fe07fb94294ee64aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4fd63aa53114589ea0e362ee1366435",
+    "url": "/DanishApp1/index.html"
+  },
+  {
+    "revision": "7791d7b9de4dd1594e8b",
+    "url": "/DanishApp1/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "31909abb14e2210a933b",
+    "url": "/DanishApp1/static/css/main.9047d2d5.chunk.css"
+  },
+  {
+    "revision": "7791d7b9de4dd1594e8b",
+    "url": "/DanishApp1/static/js/2.11eec47b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DanishApp1/static/js/2.11eec47b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31909abb14e2210a933b",
+    "url": "/DanishApp1/static/js/main.e96cc642.chunk.js"
+  },
+  {
+    "revision": "cf1d045d32e7903f318f",
+    "url": "/DanishApp1/static/js/runtime-main.49450961.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/DanishApp1/static/media/SEGA.12a3db8b.woff"
+  }
+]);
